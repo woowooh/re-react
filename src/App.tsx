645d543c310467117894a6afa7303b8352736b9e
woowooh <woowooh@outlook.com>
2023-07-04 22:06:40 +0800

    new

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello, h1 </h1>
+        <h2>Hello, h1 </h2>
+        <h3>Hello, h1 </h3>
+        <hr />
+        <code>
+          const a = 'www'
+        </code>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
